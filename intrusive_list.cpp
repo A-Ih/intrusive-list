c89afdb795a09525f2559194e4cfcf1fd8f61295
Ahmad Ibrahim <iahmd01@gmail.com>
2022-09-21 21:28:20 +0300

    first normal version; clang-format applied

--- intrusive_list.cpp
+++ intrusive_list.cpp
@@ -1,1 +1,64 @@
 #include "intrusive_list.h"
+
+namespace intrusive {
+
+list_base::list_base() = default;
+
+list_base::list_base(list_base&& other) : prev{other.prev}, next{other.next} {
+  if (other.prev != nullptr && other.next != nullptr) {
+    prev->next = this;
+    next->prev = this;
+    other.prev = other.next = nullptr;
+  }
+}
+
+list_base::list_base(const list_base&) {} // not default!
+
+list_base& list_base::operator=(list_base&& other) {
+  if (this == &other) {
+    return *this;
+  }
+  prev = other.prev;
+  next = other.next;
+  other.prev = other.next = nullptr;
+  if (other.prev != nullptr && other.next != nullptr) {
+    prev->next = this;
+    next->prev = this;
+  }
+  return *this;
+}
+
+list_base& list_base::operator=(const list_base& other) {
+  return *this;
+}
+
+void list_base::unlink() {
+  if (prev == nullptr && next == nullptr) {
+    return;
+  }
+  prev->next = next;
+  next->prev = prev;
+  prev = next = nullptr;
+}
+
+/// Insert `other` before this element
+void list_base::insert(list_base& other) {
+  assert(prev != nullptr && next != nullptr);
+  if (this == &other) {
+    // We don't want to insert the element before itself
+    return;
+  }
+  other.unlink();
+
+  prev->next = &other;
+  other.prev = this->prev;
+
+  other.next = this;
+  this->prev = &other;
+}
+
+list_base::~list_base() {
+  unlink();
+}
+
+} // namespace intrusive
--- intrusive_list.h
+++ intrusive_list.h
@@ -1,106 +1,47 @@
 #pragma once
 
 #include <cassert>
+#include <iterator>
 #include <type_traits>
 #include <utility>
-#include <iterator>
 
 namespace intrusive {
 struct default_tag;
 
-// marker for non-connected node: prev == next == nullptr
 struct list_base {
+  // NOTE: marker for non-connected node: prev == next == nullptr
   list_base* prev{nullptr};
   list_base* next{nullptr};
 
-  list_base() = default;
+  list_base();
+  list_base(list_base&& other);
+  list_base(const list_base&);
+  list_base& operator=(list_base&& other);
+  list_base& operator=(const list_base& other);
 
-  list_base(list_base&& other) : prev{other.prev}, next{other.next} {
-    if (other.prev != nullptr && other.next != nullptr) {
-      prev->next = this;
-      next->prev = this;
-      other.prev = other.next = nullptr;
-    }
-  }
-
-  list_base(const list_base&) {}  // not default!
-
-  list_base& operator=(list_base&& other) {
-    if (this == &other) {
-      return *this;
-    }
-    prev = other.prev;
-    next = other.next;
-    other.prev = other.next = nullptr;
-    if (other.prev != nullptr && other.next != nullptr) {
-      prev->next = this;
-      next->prev = this;
-    }
-    return *this;
-  }
-
-  list_base& operator=(const list_base& other) {
-    return *this;
-  }
-
-  void unlink() {
-    if (prev == nullptr && next == nullptr) {
-      return;
-    }
-    prev->next = next;
-    next->prev = prev;
-    prev = next = nullptr;
-  }
-
-  /// Insert `other` before this element
-  void insert(list_base& other) {
-    assert(prev != nullptr && next != nullptr);
-    if (this == &other) {
-      // We don't want to insert the element before itself
-      return;
-    }
-    other.unlink();
-
-    prev->next = &other;
-    other.prev = this->prev;
-
-    other.next = this;
-    this->prev = &other;
-  }
-
-  ~list_base() {
-    unlink();
-  }
+  void unlink();
+  void insert(list_base& other);
+  ~list_base();
 };
 
-template<typename Tag = default_tag>
+template <typename Tag = default_tag>
 struct list_element : public list_base {};
 
 template <typename T, typename Tag = default_tag>
 struct list {
-  static_assert(std::is_base_of_v<list_element<Tag>, T>, "T should derive from list_element<Tag>");
+  static_assert(std::is_base_of_v<list_element<Tag>, T>,
+                "T should derive from list_element<Tag>");
 
   list() {
     sentinel.prev = &sentinel;
     sentinel.next = &sentinel;
   }
 
-  list(list&& other) {
-    if (other.empty()) {
-      sentinel.prev = sentinel.next = &sentinel;
-    } else {
-      sentinel.prev = other.sentinel.prev;
-      sentinel.next = other.sentinel.next;
-    }
-    other.sentinel.prev = other.sentinel.next = &other.sentinel;
-    sentinel.prev->next = sentinel.next->prev = &sentinel;
-    assert(other.empty());
-
-    // The moved-from list is now empty and in valid state
+  list(list&& other) : list{} {
+    *this = std::move(other);
   }
 
   list& operator=(list&& other) {
-    // TODO: swap-trick
     if (this == &other) {
       return *this;
     }
@@ -108,25 +49,27 @@ struct list {
     if (other.empty()) {
       return *this;
     }
-    sentinel.next = other.sentinel.next;
     sentinel.prev = other.sentinel.prev;
-
-    sentinel.next->prev = &sentinel;
-    sentinel.prev->next = &sentinel;
+    sentinel.next = other.sentinel.next;
 
     other.sentinel.prev = &other.sentinel;
     other.sentinel.next = &other.sentinel;
 
+    sentinel.next->prev = &sentinel;
+    sentinel.prev->next = &sentinel;
+
+    assert(other.empty());
+
     return *this;
   }
 
-  template<bool Const>
+  template <bool Const>
   struct generic_iterator;
 
   using iterator = generic_iterator<false>;
   using const_iterator = generic_iterator<true>;
 
-  template<bool Const>
+  template <bool Const>
   struct generic_iterator {
     using value_type = std::conditional_t<Const, const T, T>;
     using difference_type = std::ptrdiff_t;
@@ -134,11 +77,11 @@ struct list {
     using pointer = value_type*;
     using reference = value_type&;
 
-    generic_iterator() : data{nullptr} {}
+    generic_iterator() = default;
 
     generic_iterator(const iterator& iter) : data{iter.data} {}
 
-    template<bool Dummy = Const, typename = std::enable_if_t<Dummy>>
+    template <bool Dummy = Const, typename = std::enable_if_t<Dummy>>
     generic_iterator(const const_iterator& iter) : data{iter.data} {}
 
     generic_iterator& operator=(const iterator& other) {
@@ -146,21 +89,20 @@ struct list {
       return *this;
     }
 
-    template<bool Dummy = Const, typename = std::enable_if_t<Dummy>>
+    template <bool Dummy = Const, typename = std::enable_if_t<Dummy>>
     generic_iterator& operator=(const const_iterator& other) {
       this->data = other.data;
       return *this;
     }
 
     pointer operator->() const {
-      return static_cast<value_type*>(static_cast<list_element<Tag>*>(data));
+      return static_cast<pointer>(static_cast<list_element<Tag>*>(data));
     }
 
     reference operator*() const {
-      return static_cast<value_type&>(static_cast<list_element<Tag>&>(*data));
+      return static_cast<reference>(static_cast<list_element<Tag>&>(*data));
     }
 
-    // TODO: using operator++; ???
     generic_iterator& operator++() {
       data = data->next;
       return *this;
@@ -183,21 +125,21 @@ struct list {
       return result;
     }
 
-    template<bool Const2>
-    bool operator==(const generic_iterator<Const2>& rhs) const {
+    template <bool ConstRhs>
+    bool operator==(const generic_iterator<ConstRhs>& rhs) const {
       return data == rhs.data;
     }
 
-    template<bool Const2>
-    bool operator!=(const generic_iterator<Const2>& rhs) const {
+    template <bool ConstRhs>
+    bool operator!=(const generic_iterator<ConstRhs>& rhs) const {
       return data != rhs.data;
     }
 
   private:
-    explicit generic_iterator(list_base *data_) : data{data_} {};
+    explicit generic_iterator(list_base* data_) : data{data_} {};
     friend list<T, Tag>;
 
-    list_base* data;
+    list_base* data{nullptr};
   };
 
   void push_back(T& val) noexcept {
@@ -262,8 +204,7 @@ struct list {
   }
 
   const_iterator end() const noexcept {
-    // NOTE: this is a hack to counter the constness of sentinel
-    return const_iterator{sentinel.prev->next};
+    return const_iterator{const_cast<list_element<Tag>*>(&sentinel)};
   }
 
   iterator insert(const_iterator it, T& val) noexcept {
@@ -279,10 +220,8 @@ struct list {
     return it;
   }
 
-  void splice(const_iterator pos, list& other, const_iterator first, const_iterator last) {
-    if (this == &other) {
-      // TODO
-    }
+  void splice(const_iterator pos, list& other, const_iterator first,
+              const_iterator last) {
     if (first == last) {
       return;
     }
@@ -295,26 +234,13 @@ struct list {
 
     last.data->next = pos.data;
     pos.data->prev = last.data;
-
-    // do {
-    //   // TODO: this can be done in O(1)
-    //   pos.data->insert(*first.data);
-    // } while (first != last);
   }
 
   ~list() {
     clear();
   }
 
-  // What to do if we want to insert an element that already is in the list
-  // (ours or the other)?
-  // The only special case is when we try to insert a node before itself
-  // (+kostil "if (val == iter->...) -> noop")
   list_element<Tag> sentinel;
 };
-
-// const_iterator -> don't modify the pointed-to element
-// iterator -> we can access and modify the element
-// The problem is the copypaste -> use sfinae
 
 } // namespace intrusive
